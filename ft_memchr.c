0d99e22775aa517615f45909fbbe193879838a50
Saad Karkal <sakarkal@e1r5p2.1337.ma>
2023-03-10 21:07:31 +0100

    locate byte in byte string

---
+++ ft_memchr.c
@@ -0,0 +1,12 @@
+#include "full_library.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+	size_t i;
+
+	i = -1;
+	while (++i < n)
+		if (*(unsigned char *)(s + i) == (unsigned char)c)
+			return ((void *)s + i);
+	return (NULL);
+}
